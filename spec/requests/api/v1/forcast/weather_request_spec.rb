eb95c9d62895753a55839f80fc6e958de7e32e0c
Emmy Morris <emmymorris1102@gmail.com>
2021-08-06 21:00:08 -0600

    Make test for API call

---
+++ spec/requests/api/v1/forcast/weather_request_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Map Quest API" do
+
+  context 'happy paths' do
+    it 'Send current, daily, and hourly weather report' do
+
+      get "/api/v1/forecast?location=denver,co"
+
+      body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+
+      expect(body[:data][:attributes]).to have_key(:weather_forecast)
+      expect(body[:data][:attributes][:weather_forecast]).to have_key(:current_weather)
+      expect(body[:data][:attributes][:weather_forecast][:current_weather]).to be_a(Hash)
+      expect(body[:data][:attributes][:weather_forecast]).to have_key(:daily_weather)
+      expect(body[:data][:attributes][:weather_forecast][:daily_weather]).to be_a(Array)
+      expect(body[:data][:attributes][:weather_forecast]).to have_key(:hourly_weather)
+      expect(body[:data][:attributes][:weather_forecast][:hourly_weather]).to be_a(Array)
+    end
+  end
+end
