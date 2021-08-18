721ef08e38fc1b5ce13a16c78ce7273d2973300a
Emmy Morris <emmymorris1102@gmail.com>
2021-08-18 17:00:33 -0600

    Add travis and login sad paths

--- spec/requests/api/v1/login/login_request_spec.rb
+++ spec/requests/api/v1/login/login_request_spec.rb
@@ -29,4 +29,24 @@ RSpec.describe "Sessions API" do
       expect(body[:data][:attributes][:api_key]).to eq(user.api_key)
     end
   end
+
+  describe 'Cannot create a session from invalid user' do
+   it 'Create a session when a user logs in' do
+     post '/api/v1/users', params: {
+       email: "test@example.com",
+       password: "password",
+       password_confirmation: "password"
+     }
+     body = JSON.parse(response.body, symbolize_names: true)
+     user = User.find(body[:data][:id].to_i)
+
+     post '/api/v1/sessions', params: {
+       email: "invalidtest@example.com",
+       password: "password"
+     }
+     body = JSON.parse(response.body, symbolize_names: true)
+     expect(response.status).to eq(400)
+     expect(body[:error]).to eq("Invalid Credentials.")
+   end
+ end
 end
